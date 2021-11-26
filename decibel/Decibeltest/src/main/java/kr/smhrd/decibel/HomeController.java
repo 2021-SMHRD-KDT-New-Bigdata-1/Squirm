3807bf92acb81be538add3ad9c51fd00bca5dc8d
최한글 <82924933+Naerok@users.noreply.github.com>
2021-11-26 10:18:00 +0900

    init

---
+++ decibel/Decibeltest/src/main/java/kr/smhrd/decibel/HomeController.java
@@ -0,0 +1,23 @@
+package kr.smhrd.decibel;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+
+
+	@RequestMapping("/list.do")
+	public String playlist() {
+		return "list";
+				
+	}
+	@RequestMapping("/decibel.do")
+	public String decibel() {
+		return "decibel";
+				
+	}
+
+}
