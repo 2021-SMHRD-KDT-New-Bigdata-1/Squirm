30ad2f46372db1999f0888a79402be1e6f00f3d6
최한글 <82924933+Naerok@users.noreply.github.com>
2021-11-30 11:30:33 +0900

    홈컨트롤러 띄어쓰기 충돌

--- decibel/Decibeltest/src/main/java/kr/smhrd/decibel/HomeController.java
+++ decibel/Decibeltest/src/main/java/kr/smhrd/decibel/HomeController.java
@@ -3,6 +3,7 @@ package kr.smhrd.decibel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class HomeController {
 
@@ -10,7 +11,7 @@ public class HomeController {
 	@RequestMapping("/list.do")
 	public String playlist() {
 		return "list";
-
+				
 	}
 	@RequestMapping("/decibel.do")
 	public String decibel() {
